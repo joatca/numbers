9d11ee867f9e078fa322a933ef22867a314cd268
Fraser McCrossan <fraser@mccrossan.ca>
2019-10-05 19:13:57 -0400

    bugfix: the naïve generation of combinations when there are duplicate values gnerates a lot of duplicates; use a stupid (but good and faster enough) method to eliminate them

--- all-games.cr
+++ all-games.cr
@@ -1,7 +1,11 @@
+used = Set(String).new
 
 ((1..10).to_a * 2 + [25, 50, 75, 100]).sort.each_combination(6) do |c|
   (100..999).each do |target|
-    c.join(" ", STDOUT)
-    STDOUT << " " << target << '\n'
+    s = c.join(" ") + " " + target.to_s
+    unless used.includes?(s)
+      STDOUT << s << '\n'
+      used << s
+    end
   end
 end
